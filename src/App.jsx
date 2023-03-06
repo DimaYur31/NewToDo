b3b2b3d41ed047062cfd865393a6b73bab1dd704
DimaYur31 <d_yurchik@bk.ru>
2023-03-06 19:36:11 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from './components/header/Header';
 import { SideBar } from './components/sidebar/SideBar';
 import { Content } from './components/content/Content';
-import { Layout } from './components/layout/layout';
+import { Layout } from './components/layout/Layout';
 
 export const App = () => {
 	return (
